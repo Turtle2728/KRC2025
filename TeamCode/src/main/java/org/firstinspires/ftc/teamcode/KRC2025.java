ac48f7dab993da84fbec9e70bff03a61b92cb1fc
Turtle2728 <qzwxecrv0308@gmail.com>
2025-01-19 17:14:57 +0900

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KRC2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KRC2025.java
@@ -25,7 +25,7 @@ public class KRC2025 extends LinearOpMode {
     private PIDController controller;
 
     public static double p = 0.01, i = 0, d = 0.0005; //초기값 0.005
-    public static double f = 0.2;//초기값 0.28
+    public static double f = 0.28;//초기값 0.28
     public static double maxSpeed = 0.9; // 최대 속도 제한
     public static double maxAccel = 0.1; // 최대 가속도 제한
     public static double sCurveRampRate = 0.04; // S-curve 변화율 (0 ~ 1)
@@ -41,8 +41,13 @@ public class KRC2025 extends LinearOpMode {
     private final int targetG = 100; // 휴먼플레이어가 주는 기물 잡는 높이
     private final int targetGUP = 300; // 걸린 기물 잡고 팔 올리는 값
     private final int LenghUP = 4000; // 2층 바구니로 팔 올리는 값
+    private final double currentwristL = 0;
+    private final double currentwristR = 0;
+
+
+    private final int LenghPick = 2500; // 잠수정 기물 픽 길이
     private final int LenghDown = 0; // 팔 길이 최소값
-    private final int targetSUP = 50; //팔 각도 약간 올리는 값
+    private final int targetSUP = 80; //팔 각도 약간 올리는 값
     private final double wristDown = 0.75;
     private final double wristUP = 0.22;
     private final double wristMiddle = 0.5;
@@ -108,13 +113,12 @@ public class KRC2025 extends LinearOpMode {
         waitForStart();
 
 
-
         int targetLengh = 0;
         int currentLengh = 0;
 
         double WlPosion = 0;
         double WrPosion = 0;
-        double interval = 0.05;
+        double interval = 0.04;
 
         gripper.setPosition(gripClose);
         wristL.setPosition(0.22);
@@ -122,7 +126,6 @@ public class KRC2025 extends LinearOpMode {
 
         AL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         AL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
 
 
         while (opModeIsActive()) {
@@ -154,13 +157,13 @@ public class KRC2025 extends LinearOpMode {
             double leftFrontPower = ((rotY + rotX + R) / denominator) * slow;
             double leftBackPower = ((rotY - rotX + R) / denominator) * slow;
             double rightFrontPower = ((rotY - rotX - R) / denominator) * slow;
-            double rightBackPower  = ((rotY + rotX - R) / denominator) * slow;
+            double rightBackPower = ((rotY + rotX - R) / denominator) * slow;
 
             leftFront.setPower(leftFrontPower);
             leftBack.setPower(leftBackPower);
             rightFront.setPower(rightFrontPower);
             rightBack.setPower(rightBackPower);
-
+// !!!!!플레이어 1
             //gripper open/close
             if (gamepad1.right_bumper) {
                 gripper.setPosition(gripOepn);
@@ -169,15 +172,11 @@ public class KRC2025 extends LinearOpMode {
             }
 
             if (gamepad1.left_bumper) {
-                wristL.setPosition(wristMiddle);
-                wristR.setPosition(wristMiddle);
+                target = targetDown; // 팔 최대 내리기
+                wrist_control(wristL.getPosition(),wristR.getPosition());
             }
 
-            //준비
-            if (gamepad1.y) {
-                wristL.setPosition(wristUP);
-                wristR.setPosition(wristUP);
-            }
+
 
             //손목 반시계 방향으로 돌리기
             if (rising_edge(currentGamepad1.x, previousGamepad1.x)) {
@@ -195,7 +194,7 @@ public class KRC2025 extends LinearOpMode {
             }
 
             //손목 시계 방향으로 돌리기
-            if (rising_edge(currentGamepad1.b, previousGamepad1.b)){
+            if (rising_edge(currentGamepad1.b, previousGamepad1.b)) {
                 WlPosion = wristL.getPosition() - interval;
                 WrPosion = wristR.getPosition() + interval;
 
@@ -210,121 +209,154 @@ public class KRC2025 extends LinearOpMode {
             }
 
 
-            //구조물 안의 기물 잡기
+            //휴먼 플레이어가 준 기물 잡고 팔 올리는 값
             if (gamepad1.a) {
-                wristL.setPosition(wristDown);
-                wristR.setPosition(wristDown);
+                target = targetGUP;
             }
 
-            if (gamepad1.dpad_left) {
-                targetLengh = currentLengh + 100;
+            //휴먼 플레이어가 준 기물 잡는 높이
+            if (gamepad1.y) {
+                target = targetSUP;
+            }
+
+//pick up position
+            if (gamepad1.dpad_up) {
+                targetLengh = LenghPick;
                 AL.setTargetPosition(targetLengh);
                 AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 AL.setPower(1);
                 currentLengh = AL.getCurrentPosition();
+                target = targetSUP;
+                wrist_control(wristMiddle,wristMiddle);
             }
-
-            if (gamepad1.dpad_right) {
-                if (targetLengh>10) {
-                    targetLengh =currentLengh - 100;
+//deposit position
+                if (gamepad1.dpad_down) {
+                    targetLengh = LenghDown;
                     AL.setTargetPosition(targetLengh);
                     AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     AL.setPower(1);
                     currentLengh = AL.getCurrentPosition();
-                } else {
-                    targetLengh = 10;
+                    target = targetDown;
+                    wristL.setPosition(wristMiddle);
+                    wristR.setPosition(wristMiddle);
+                }
+                if (gamepad1.dpad_left) {
+                    wrist_control(wristDown,wristDown);
+                }
+
+                if (gamepad1.dpad_right) {
+                    target = targetG;
+                }
+
+// !!!!!!플레이어 2
+
+//팔각도 최소
+
+                //팔길이 늘리기
+                if (gamepad2.dpad_up) {
+                    targetLengh = currentLengh + 100;
                     AL.setTargetPosition(targetLengh);
                     AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     AL.setPower(1);
                     currentLengh = AL.getCurrentPosition();
                 }
+//팔길이 줄이기
+                if (gamepad2.dpad_down) {
+                    if (targetLengh > 10) {
+                        targetLengh = currentLengh - 100;
+                        AL.setTargetPosition(targetLengh);
+                        AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        AL.setPower(1);
+                        currentLengh = AL.getCurrentPosition();
+                    } else {
+                        targetLengh = 10;
+                        AL.setTargetPosition(targetLengh);
+                        AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        AL.setPower(1);
+                        currentLengh = AL.getCurrentPosition();
+                    }
+                }
+                if (gamepad2.y) {
+                    target = targetUp;
+                    wrist_control(wristUP,wristUP);
+
+                } else if (gamepad2.a & currentLengh < 500) {
+                    target = targetDown;
+                    wristL.setPosition(wristMiddle);
+                    wristR.setPosition(wristMiddle);
+                }
+
+                if (gamepad2.x) {
+                    targetLengh = LenghDown;
+                    AL.setTargetPosition(targetLengh);
+                    AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    AL.setPower(1);
+                } else if (gamepad2.b) {
+                    target = targetC;
+                    wristL.setPosition(0.25);
+                    wristR.setPosition(0.75);
+                }
+
+                if (gamepad2.dpad_left) {
+                    targetLengh = LenghUP;
+                    AL.setTargetPosition(targetLengh);
+                    AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    AL.setPower(1);
+                }
+
+                if (gamepad2.dpad_right) {
+                    targetLengh = LenghDown;
+                    AL.setTargetPosition(targetLengh);
+                    AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    AL.setPower(1);
+                }
+
+
+                double error = target - currentTarget;
+
+                double increment = sCurveRampRate * error;
+                if (Math.abs(increment) > Math.abs(error)) {
+                    increment = error;
+                }
+                currentTarget += increment;
+
+                int armPos = AF.getCurrentPosition();
+                double pid = controller.calculate(armPos, currentTarget);
+                double ff = Math.cos(Math.toRadians(armPos / ticks_in_degree_AA)) * f;
+
+                double rawPower = pid + ff;
+                rawPower = Math.max(-maxSpeed, Math.min(maxSpeed, rawPower));
+
+                double powerChange = rawPower - previousPower;
+                if (Math.abs(powerChange) > maxAccel) {
+                    powerChange = Math.signum(powerChange) * maxAccel;
+                }
+
+                double finalPower = previousPower + powerChange;
+                previousPower = finalPower;
+
+                AF.setPower(finalPower);
+                AB.setPower(finalPower);
+
+                telemetry.addData("gripper", gripper.getPosition());
+                telemetry.addData("wristL", wristL.getPosition());
+                telemetry.addData("wristR", wristR.getPosition());
+                telemetry.addData("pos_AL ", AL.getCurrentPosition());
+                telemetry.addData("Current Target", currentTarget);
+                telemetry.addData("Position", armPos);
+                telemetry.addData("Target", target);
+                telemetry.addData("Power", finalPower);
+                telemetry.update();
             }
-
-            if (gamepad2.dpad_up) {
-                targetLengh = LenghUP;
-                AL.setTargetPosition(targetLengh);
-                AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                AL.setPower(1);
-                currentLengh = AL.getCurrentPosition();
-            }
-
-            if (gamepad2.dpad_down) {
-                targetLengh = LenghDown;
-                AL.setTargetPosition(targetLengh);
-                AL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                AL.setPower(1);
-                currentLengh = AL.getCurrentPosition();
-            }
-
-            if (gamepad2.y) {
-                target = targetUp;
-            } else if (gamepad2.a) {
-                target = targetDown;
-            }
-
-            if (gamepad2.x) {
-                target = targetG;
-            } else if (gamepad2.b) {
-                target = targetC;
-                wristL.setPosition(0.25);
-                wristR.setPosition(0.75);
-            }
-
-            if (gamepad2.dpad_left) {
-                target = targetGUP;
-            }
-
-            if (gamepad2.dpad_right) {
-                target = targetSUP;
-            }
-
-
-
-
-            double error = target - currentTarget;
-
-            double increment = sCurveRampRate * error;
-            if (Math.abs(increment)>Math.abs(error)) {
-                increment = error;
-            }
-            currentTarget += increment;
-
-            int armPos = AF.getCurrentPosition();
-            double pid = controller.calculate(armPos,currentTarget);
-            double ff = Math.cos(Math.toRadians(armPos/ticks_in_degree_AA)) * f;
-
-            double rawPower = pid + ff;
-            rawPower = Math.max(-maxSpeed, Math.min(maxSpeed,rawPower));
-
-            double powerChange = rawPower - previousPower;
-            if (Math.abs(powerChange)>maxAccel) {
-                powerChange = Math.signum(powerChange) * maxAccel;
-            }
-
-            double finalPower = previousPower + powerChange;
-            previousPower = finalPower;
-
-            AF.setPower(finalPower);
-            AB.setPower(finalPower);
-
-            telemetry.addData("gripper", gripper.getPosition());
-            telemetry.addData("wristL", wristL.getPosition());
-            telemetry.addData("wristR",wristR.getPosition());
-            telemetry.addData("pos_AL ", AL.getCurrentPosition());
-            telemetry.addData("Current Target", currentTarget);
-            telemetry.addData("Position", armPos);
-            telemetry.addData("Target", target);
-            telemetry.addData("Power", finalPower);
-            telemetry.update();
         }
-    }
-    private void wrist_control(double L, double R) {
-        wristL.setPosition(L);
-        wristR.setPosition(R);
-    }
 
-    private boolean rising_edge(boolean currentButtonState, boolean previousButtonState) {
-        return currentButtonState && !previousButtonState;
-    }
+        private void wrist_control ( double L, double R){
+            wristL.setPosition(L);
+            wristR.setPosition(R);
+        }
 
-}
+        private boolean rising_edge ( boolean currentButtonState, boolean previousButtonState){
+            return currentButtonState && !previousButtonState;
+        }
+
+    }
